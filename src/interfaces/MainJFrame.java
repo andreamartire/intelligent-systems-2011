229bb8661db4a83721679dc6f35fd6df2c9f0db9
andreamartire@gmail.com <andreamartire@gmail.com@b76ab2fb-e43b-203b-3b45-ef96717101a3>
2011-10-31 13:36:53 +0000

    review

--- src/interfaces/MainJFrame.java
+++ src/interfaces/MainJFrame.java
@@ -33,26 +33,14 @@ public class MainJFrame extends javax.swing.JFrame {
 	private JMenuBar jMenuBar;
 	private JMenu menuFile;
 	private JMenuItem closeMenuItem;
-	private JPanel commandPanel;
-	private JButton StopButton;
-	private JButton startButton;
-	private JButton generatorButton;
-	private JTextField dirtField;
-	private JLabel dirtLabel;
-	private JTextField obstaclesField;
-	private JLabel obstaclesLabel;
-	private JPanel eastPanel;
-	private JLabel widthLabel;
-	private JPanel GenerationPanel;
-	private JButton refreshButton;
-	private JTextField jTextField1;
-	private JTextField lenghtField;
-	private JLabel lengthLabel;
-	private JPanel dimensionPanel;
-	private JPanel westPanel;
-	private JLabel[][] labelMatrix;
-	private ImageIcon dirtIcon, obstacleIcon, tileIcon, vacuumIcon;
+	
 	int iconSize = 60;
+	
+	private GridPanel gridPanel;
+	private SettingsPanel settingsPanel;
+	
+	Environment myEnv;
+	Agent agent;
 	
 	public static void main(String[] args) {
 		new MainJFrame();
@@ -90,108 +78,28 @@ public class MainJFrame extends javax.swing.JFrame {
 				}
 			}
 		}
-		{
-			westPanel = new JPanel();
-			getContentPane().add(westPanel);
-			GridBagLayout jPanel2Layout = new GridBagLayout();
-			westPanel.setBackground(Color.RED);
-			westPanel.setSize(250, iconSize*7);
-			jPanel2Layout.rowWeights = new double[] {0.1, 0.1, 0.1};
-			jPanel2Layout.rowHeights = new int[] {7, 7, 7};
-			jPanel2Layout.columnWeights = new double[] {0.1};
-			jPanel2Layout.columnWidths = new int[] {7};
-			westPanel.setLayout(jPanel2Layout);
-			{
-				dimensionPanel = new JPanel();
-				
-				Border marginOutside = new EmptyBorder(10,10,10,10);        
-		        TitledBorder title = BorderFactory.createTitledBorder("Size Settings");
-		        CompoundBorder upperBorder = new CompoundBorder(marginOutside, title);
-		        Border marginInside = new EmptyBorder(10,10,10,10);
-		        dimensionPanel.setBorder(new CompoundBorder(upperBorder, marginInside));
-				
-				westPanel.add(dimensionPanel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-				lengthLabel = new JLabel();
-				dimensionPanel.add(lengthLabel);
-				lengthLabel.setText("Lenght");
-				lenghtField = new JTextField();
-				dimensionPanel.add(lenghtField);
-				lenghtField.setText("7");
-				widthLabel = new JLabel();
-				dimensionPanel.add(widthLabel);
-				widthLabel.setText("Width");
-				jTextField1 = new JTextField();
-				dimensionPanel.add(jTextField1);
-				jTextField1.setText("5");
-				refreshButton = new JButton();
-				dimensionPanel.add(refreshButton);
-				refreshButton.setText("Refresh");
-			}
-			{
-				GenerationPanel = new JPanel();
-				
-				Border marginOutside = new EmptyBorder(10,10,10,10);        
-		        TitledBorder title = BorderFactory.createTitledBorder("Build Settings");
-		        CompoundBorder upperBorder = new CompoundBorder(marginOutside, title);
-		        Border marginInside = new EmptyBorder(10,10,10,10);
-		        GenerationPanel.setBorder(new CompoundBorder(upperBorder, marginInside));
-		        
-				westPanel.add(GenerationPanel, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-				{
-					obstaclesLabel = new JLabel();
-					GenerationPanel.add(obstaclesLabel);
-					obstaclesLabel.setText("Obstacles");
-					obstaclesField = new JTextField();
-					GenerationPanel.add(obstaclesField);
-					obstaclesField.setText("8");
-					dirtLabel = new JLabel();
-					GenerationPanel.add(dirtLabel);
-					dirtLabel.setText("Dirt");
-					dirtField = new JTextField();
-					GenerationPanel.add(dirtField);
-					dirtField.setText("7");
-
-					generatorButton = new JButton();
-					GenerationPanel.add(generatorButton);
-					generatorButton.setText("Generate");
-				}
-			}
-			{
-				commandPanel = new JPanel();
-				
-				Border marginOutside = new EmptyBorder(10,10,10,10);        
-		        TitledBorder title = BorderFactory.createTitledBorder("Commands");
-		        CompoundBorder upperBorder = new CompoundBorder(marginOutside, title);
-		        Border marginInside = new EmptyBorder(10,10,10,10);
-		        commandPanel.setBorder(new CompoundBorder(upperBorder, marginInside));
-				
-				westPanel.add(commandPanel, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-				{
-					startButton = new JButton();
-					commandPanel.add(startButton);
-					startButton.setText("Start");
-					StopButton = new JButton();
-					commandPanel.add(StopButton);
-					StopButton.setText("Stop");
-				}
-			}
-		}
-		{
-			eastPanel = new JPanel();
-			getContentPane().add(eastPanel);
-			eastPanel.setSize(290,290);
-		}
+		
+		settingsPanel = new SettingsPanel();
+		getContentPane().add(settingsPanel);
+		settingsPanel.setSize(290,290);
+		
+		int l = 7, w = 7;
+		agent = new Agent(0,0,l,w,Agent.VisibilityType.MY_NEIGHBOURS);
+		myEnv = new Environment(l,w,agent,DynamicType.STATIC);
+		
+		gridPanel = new GridPanel(myEnv, iconSize);
+		getContentPane().add(gridPanel);
+		gridPanel.setSize(290,290);
+		
 		pack();
 		this.setVisible(true);
 	}
-	
+
 	public void init(){
-		int l = 7, w = 7;
-		Agent agent = new Agent(0,0,l,w,Agent.VisibilityType.MY_NEIGHBOURS);
-		Environment myEnv = Environment.create(l,w,agent,DynamicType.STATIC);
+		
+		
 		myEnv.show();
-		interfaceInit(myEnv);
-		interfaceUpdate(myEnv);
+		gridPanel.gridPanelUpdate(myEnv);
 		while(!agent.goalReached() && agent.actionList.size()<myEnv.opBound){
 			agent.perceives(myEnv.getPerceptions());
 			agent.update();
@@ -199,7 +107,7 @@ public class MainJFrame extends javax.swing.JFrame {
 			System.out.println("Action received: " + myEnv.currAction);
 			myEnv.update();
 			myEnv.show();
-			interfaceUpdate(myEnv);
+			gridPanel.gridPanelUpdate(myEnv);
 			System.out.println("-------------------");
 		}
 		System.out.println("Num actions: " + agent.actionList.size());
@@ -208,47 +116,5 @@ public class MainJFrame extends javax.swing.JFrame {
 		System.out.println("-- End --");
 	}
 
-	private void interfaceInit(Environment myEnv) {
-		eastPanel.setLayout(new GridLayout(myEnv.lenght, myEnv.width));
-		eastPanel.setBackground(Color.green);
-		
-		dirtIcon = new ImageIcon(new ImageIcon("img/dirt.jpeg").getImage().getScaledInstance(iconSize,iconSize,iconSize));
-		obstacleIcon = new ImageIcon(new ImageIcon("img/wall.jpeg").getImage().getScaledInstance(iconSize,iconSize,iconSize));
-		tileIcon = new ImageIcon(new ImageIcon("img/tile.jpeg").getImage().getScaledInstance(iconSize,iconSize,iconSize));
-		vacuumIcon = new ImageIcon(new ImageIcon("img/vacuum.jpeg").getImage().getScaledInstance(iconSize,iconSize,iconSize));
-		
-		labelMatrix = new JLabel[myEnv.lenght][myEnv.width];
-		
-		GridBagConstraints constraints = new GridBagConstraints();
-		for(int i=0; i<myEnv.lenght; i++)
-			for(int j=0; j<myEnv.width; j++){
-				constraints.fill = GridBagConstraints.BOTH;
-				constraints.gridx = i;
-				constraints.gridy = j;
-				JLabel label = new JLabel();
-				labelMatrix[i][j] = label;
-				eastPanel.add(label, constraints);
-			}
-	}
-
-	private void interfaceUpdate(Environment myEnv) {
-		for(int i=0; i<myEnv.lenght; i++)
-			for(int j=0; j<myEnv.width; j++){
-				if(myEnv.floor.get(i,j) == Square.Type.DIRTY)
-					labelMatrix[i][j].setIcon(dirtIcon);
-				if(myEnv.floor.get(i,j) == Square.Type.CLEAN)
-					labelMatrix[i][j].setIcon(tileIcon);
-				if(myEnv.floor.get(i,j) == Square.Type.OBSTACLE)
-					labelMatrix[i][j].setIcon(obstacleIcon);
-				if(myEnv.agent.x == i && myEnv.agent.y == j)
-					labelMatrix[i][j].setIcon(vacuumIcon);
-			}
-		repaint();
-		try {
-			Thread.sleep(250);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	
 }
